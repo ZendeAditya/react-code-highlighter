c61e41e8fb03fab34d3c86010c3e1b42d8f7c9fa
ZendeAditya <adityazende6710@gmail.com>
2024-06-28 21:06:49 +0530

    readme update

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -1,6 +1,9 @@
 "use state";
 import React, { useState } from "react";
 import Editor, { lineNumber } from "react-prism-editor";
+const code = `"use state";
+import React, { useState } from "react";
+import Editor, { lineNumber } from "react-prism-editor";
 
 const CodeEditor = () => {
   const [code, setCode] = useState("");
@@ -8,7 +11,26 @@ const CodeEditor = () => {
     <div className="px-2 lg:w-[50rem] h-auto lg:h-[50rem] overflow-y-auto overflow-x-hidden">
       <Editor
         language="jsx"
-        defaultCode={`console.log("Hello world!")`}
+        defaultCode="print('Hello world!')"
+        theme="default"
+        lineNumber={lineNumber}
+        readOnly={false}
+        clipboard={true}
+        changeCode={(newCode) => setCode(newCode)}
+      />
+    </div>
+  );
+};
+
+export default CodeEditor;
+`;
+const CodeEditor = () => {
+  const [code, setCode] = useState("");
+  return (
+    <div className="px-2 lg:w-[50rem] h-auto lg:h-[50rem] overflow-y-auto overflow-x-hidden">
+      <Editor
+        language="jsx"
+        defaultCode={code}
         theme="default"
         lineNumber={lineNumber}
         readOnly={false}
